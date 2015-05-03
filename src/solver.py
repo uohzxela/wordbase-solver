d9e4991f504fee57648414d2b53895a9b4a39e0f
alex <uohzxela@gmail.com>
2015-05-03 19:11:48 +0800

    specify player mode as command line param for solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,13 +1,14 @@
 import extractor
 import tst
+import sys
+
 WHITE = 0
 ORANGE = 1
 BLUE = 2
 BLACK = 3
+PLAYER_MAP = {'orange': ORANGE, 'blue': BLUE}
 
-player = BLUE
-
-def make_graph(matrix, color_map):
+def make_graph(matrix, color_map, player):
 	root = None
 	graph = {root: set() }
 
@@ -50,13 +51,21 @@ def find_words(graph, matrix, position, prefix, results, dictionary):
 			child_prefix = list(prefix)
 			find_words(graph, matrix, child, child_prefix, results, dictionary)
 	return
-
-matrix, color_map = extractor.get_matrix("wordbase.png")
-# matrix = [['C', 'A'], ['T', 'B']]
-dictionary = tst.TernarySearchTree()
-graph = make_graph(matrix, color_map)
-res = set()
-find_words(graph, matrix, None, [], res, dictionary)
-
-print res 
-print color_map
+	
+def main():
+	try:
+		if len(sys.argv) == 2:
+			player = PLAYER_MAP[sys.argv[1].lower()]
+			matrix, color_map = extractor.get_matrix("wordbase.png")
+			dictionary = tst.TernarySearchTree()
+			graph = make_graph(matrix, color_map, player)
+			res = set()
+			find_words(graph, matrix, None, [], res, dictionary)
+			print res
+		else:
+			raise Exception("Wrong number of arguments!")
+	except:
+		sys.stderr.write('Usage: python solver.py [blue/orange]\n')
+		exit(2)
+if __name__ == '__main__':
+    main()
